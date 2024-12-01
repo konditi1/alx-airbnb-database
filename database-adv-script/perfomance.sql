c5aa04be3ab808db342f3d5511419f62dce67877
konditi1 <konditi1@gmail.com>
2024-12-01 20:42:18 +0300

    Query optimize

--- database-adv-script/perfomance.sql
+++ database-adv-script/perfomance.sql
@@ -25,34 +25,7 @@ INNER JOIN
 LEFT JOIN 
     Payment pm ON b.booking_id = pm.booking_id;
 
-EXPLAIN 
-SELECT 
-    b.booking_id,
-    b.start_date,
-    b.end_date,
-    b.booking_status,
-    u.user_id,
-    u.first_name,
-    u.last_name,
-    u.email,
-    p.property_id,
-    p.name AS property_name,
-    p.price_per_night,
-    p.availability_status,
-    pm.payment_id,
-    pm.status,
-    pm.amount AS payment_amount
-FROM 
-    Booking b
-INNER JOIN 
-    User u ON b.user_id = u.user_id
-INNER JOIN 
-    Property p ON b.property_id = p.property_id
-LEFT JOIN 
-    Payment pm ON b.booking_id = pm.booking_id;
-
-
-SELECT 
+EXPLAIN SELECT 
     b.booking_id,
     b.start_date,
     b.end_date,
@@ -78,39 +51,8 @@ LEFT JOIN
     (
         SELECT booking_id, payment_id, status, amount
         FROM Payment
-        WHERE status IS NOT NULL  
+        WHERE status IS NOT NULL
     ) pm ON b.booking_id = pm.booking_id
 WHERE 
-    b.booking_status != 'canceled';
-
-EXPLAIN 
-SELECT 
-    b.booking_id,
-    b.start_date,
-    b.end_date,
-    b.booking_status,
-    u.user_id,
-    u.first_name,
-    u.last_name,
-    u.email,
-    p.property_id,
-    p.name AS property_name,
-    p.price_per_night,
-    p.availability_status,
-    pm.payment_id,
-    pm.status AS payment_status,
-    pm.amount AS payment_amount
-FROM 
-    Booking b
-INNER JOIN 
-    User u ON b.user_id = u.user_id
-INNER JOIN  
-    Property p ON b.property_id = p.property_id
-LEFT JOIN 
-    (
-        SELECT booking_id, payment_id, status, amount
-        FROM Payment
-        WHERE status IS NOT NULL  
-    ) pm ON b.booking_id = pm.booking_id    
-WHERE 
-    b.booking_status != 'canceled';
+    b.booking_status != 'canceled'
+    AND b.start_date > '2024-01-01';
